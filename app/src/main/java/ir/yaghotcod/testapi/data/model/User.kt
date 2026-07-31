0aadddbca528b72a497adcc488a7c3dbf6ff22f1
stevenfaster-oss <stevenfaster13@gmail.com>
2026-07-31 12:59:45 +0330

    add user model

--- app/src/main/java/ir/yaghotcod/testapi/data/model/User.kt
+++ app/src/main/java/ir/yaghotcod/testapi/data/model/User.kt
@@ -1,2 +1,11 @@
 package ir.yaghotcod.testapi.data.model
 
+object User {
+    data class User(
+        val id: Int,
+        val name: String,
+        val username: String,
+        val email: String,
+        val phone: String
+    )
+}
